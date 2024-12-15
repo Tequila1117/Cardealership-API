4d8c81435f9f64f92d12c84a3a2806aa5ce0c9d6
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-12-15 17:48:29 -0500

    created method to find vehicles by color

--- src/main/java/com/pluralsight/dealership/DAO/VehicleDAO.java
+++ src/main/java/com/pluralsight/dealership/DAO/VehicleDAO.java
@@ -305,6 +305,7 @@ public class VehicleDAO {
 
 
     }
+
     // Retrieve by color
     public List<Vehicle> findVehiclesByColor(String carColor) {
         List<Vehicle> vehicles = new ArrayList<>();
@@ -341,6 +342,7 @@ public class VehicleDAO {
         }
         return vehicles;
     }
+
     // Retrieve vehicle by odometer (mileage)
     public List<Vehicle> findVehiclesByMiles(int minMiles, int maxMiles) {
         List<Vehicle> vehicles = new ArrayList<>();
@@ -380,8 +382,46 @@ public class VehicleDAO {
         return vehicles;
 
     }
+
+    //Retrieve by vehicle type
+    public List<Vehicle> findVehiclesByType(String carType) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String vin, make, model, color, vehicleType;
+        double price;
+        int year, odometer;
+        boolean sold;
+        String query = """
+                SELECT vin, year, make, model, color, vehicle_type, sold
+                FROM vehicles
+                WHERE vehicle_type = ? 
+                """;
+        try (Connection connection = datasource.getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, carType);
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                vin = rs.getString("vin");
+                year = rs.getInt("year");
+                make = rs.getString("make");
+                model = rs.getString("model");
+                price = rs.getDouble("price");
+                color = rs.getString("color");
+                odometer = rs.getInt("odometer");
+                vehicleType = rs.getString("vehicle_type");
+                sold = rs.getBoolean("sold");
+                if (!sold) {
+                    vehicles.add(new Vehicle(vin, year, price, make, model, color, odometer, sold, vehicleType));
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return vehicles;
+    }
 }
 
-// retrieve vehicle by odometer (mileage)
 
-//Retrieve by vehicle type
+
+
